6e122943910467b8a509b343148dab593f9b676f
Dan Arbelo <govepitr@gmail.com>
2021-05-02 10:32:19 -0700

    initial commit

---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <section>
+      <h1 className="text-indigo-500" id="Nav">Look a Nav!</h1>
+    </section>
+  );
+}
+
+export default Nav;
