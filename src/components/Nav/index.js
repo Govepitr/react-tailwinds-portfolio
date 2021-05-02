2df4ce7e7b227e5c2b5f5cac127ecb84b9886f1d
Dan Arbelo <govepitr@gmail.com>
2021-05-02 10:49:38 -0700

    "killed" my children.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,37 @@ import React from 'react';
 function Nav() {
   return (
     <section>
-      <h1 className="text-indigo-500" id="Nav">Look a Nav!</h1>
+      <nav className="navbar">
+        <div className="container-fluid">
+          <span className="navbar-brand" href="https://govepitr.github.io/"></span>
+          <button className="" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse">
+            <ul>
+              <li className="nav-item">
+                <span className="nav-link active" aria-current="page" href="#about-me">About Me</span>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link active" aria-current="page" href="#work">Work</span>
+              </li>
+              <li className="nav-item dropdown">
+                <span className="nav-link" href="#links" role="button">
+                  Links
+                </span>
+                <ul id="navMenu" className="dropdown-menu">
+                  <li>
+                    <span className="dropdown-item" title="Github" href="https://github.com/Govepitr"></span>
+                  </li>
+                  <li>
+                    <span className="dropdown-item" title="LinkedIn" href="https://www.linkedin.com/in/dan-arbelo/"></span>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </section>
   );
 }
