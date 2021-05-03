c541403e344ac1dd9ecedcc8eaea6f4ce9a1cbaf
Dan Arbelo <govepitr@gmail.com>
2021-05-02 21:54:09 -0700

    Hero image positioned perfectly

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,10 @@ import AllMight from '../../../src/assets/images/Victory_1.svg';
 function Header() {
   return (
     <section>
-      <div>
+      <div className="w-1/2 h-1/2 mx-auto">
         <hero className="All-might">
-          <div className="text-right hero-text marker w-1/5 h-1/5" data-color="234, 225, 210">
-            <img className="justify-center" src={AllMight} alt="Pillar" />
+          <div className="text-right hero-text marker w-2/5 h-2/5 mx-auto" data-color="234, 225, 210">
+            <img className="" src={AllMight} alt="Pillar" />
             <h3 className="heroic">I code, therefore I am..</h3>
           </div>
         </hero>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,39 +2,65 @@ import React from 'react';
 
 function Nav() {
   return (
-    <section>
-      <nav className="navbar">
-        <div className="container-fluid">
-          <span className="navbar-brand" href="https://govepitr.github.io/"></span>
-          <button className="" type="button">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse">
+    <>
+      
+        <nav classNameName="navbar navbar-expand-lg navbar-light">
+          <div classNameName="container-fluid">
+            <span classNameName="navbar-brand" href="https://govepitr.github.io/"></span>
+            <button classNameName="navbar-toggler" type="button">
+              <span classNameName="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ml-auto mb-2 mb-lg-0 pl-5">
+                <li className="nav-item">
+                  <span className="nav-link active" aria-current="page" href="#about-me">About Me</span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link active" aria-current="page" href="#work">Work</span>
+                </li>
+                <li className="nav-item dropdown">
+                  <span className="nav-link dropdown-toggle" href="#links" id="navbarDropdown" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Links
+            </span>
+                  <ul id="navMenu" className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <span className="dropdown-item" title="Github" target="_blank" href="https://github.com/Govepitr"><i
+                        className="fa fa-github fa-2x "></i></span>
+                    </li>
+                    <li>
+                      <span className="dropdown-item" title="LinkedIn" target="_blank"
+                        href="https://www.linkedin.com/in/dan-arbelo/"><i className="fa fa-linkedin fa-2x"></i></span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
             <ul>
-              <li className="nav-item">
-                <span className="nav-link active" aria-current="page" href="#about-me">About Me</span>
+              <li classNameName="nav-item">
+                <span classNameName="nav-link active" aria-current="page" href="#about-me">About Me</span>
               </li>
-              <li className="nav-item">
-                <span className="nav-link active" aria-current="page" href="#work">Work</span>
+              <li classNameName="nav-item">
+                <span classNameName="nav-link active" aria-current="page" href="#work">Work</span>
               </li>
-              <li className="nav-item dropdown">
-                <span className="nav-link" href="#links" role="button">
+              <li classNameName="nav-item dropdown">
+                <span classNameName="nav-link" href="#links" role="button">
                   Links
                 </span>
-                <ul id="navMenu" className="dropdown-menu">
+                <ul id="navMenu" classNameName="dropdown-menu">
                   <li>
-                    <span className="dropdown-item" title="Github" href="https://github.com/Govepitr"></span>
+                    <span classNameName="dropdown-item" title="Github" href="https://github.com/Govepitr"></span>
                   </li>
                   <li>
-                    <span className="dropdown-item" title="LinkedIn" href="https://www.linkedin.com/in/dan-arbelo/"></span>
+                    <span classNameName="dropdown-item" title="LinkedIn" href="https://www.linkedin.com/in/dan-arbelo/"></span>
                   </li>
                 </ul>
               </li>
             </ul>
           </div>
-        </div>
-      </nav>
-    </section>
+        </nav>
+      
+    </>
   );
 }
 
