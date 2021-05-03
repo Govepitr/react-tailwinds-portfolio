3799c8a637ffa86eb75830e9d97f3cb1bed4a20a
Dan Arbelo <govepitr@gmail.com>
2021-05-03 04:07:24 -0700

    Netflix kinda working

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,6 +5,9 @@ import FCB from '../../assets/images/FCB.gif';
 import Cinema from '../../assets/images/cinemasearch.gif';
 import MK1 from '../../assets/images/portfolioV1.gif';
 import ComingSoon from '../../assets/images/comingsoon.gif';
+import Pardon from '../../assets/images/pardon.gif';
+import Who from '../../assets/images/who.gif';
+import Shards from '../../assets/images/muchwow.gif';
 
 function Project() {
   return (
@@ -13,45 +16,19 @@ function Project() {
       <div className="body-title text-center marker mt-5 mb-5" data-color="67, 107, 131">
         <h2>My work</h2>
       </div>
-      <div id="carouselControls" className="carousel slide mx-auto" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active" id="run-buddy">
-            <span target="_blank" href="https://govepitr.github.io/run-buddy/">
-              <img alt="Run Buddy" src={RB} />
-            </span>
-          </div>
-          <div className="carousel-item" id="flash-card">
-            <span target="_blank" href="https://flash-card-buddy.herokuapp.com/">
-              <img alt="FCB" src={FCB} />
-            </span>
-          </div>
-          <div className="carousel-item" id="horiseon">
-            <span target="_blank" href="https://govepitr.github.io/Horiseon/">
-              <img alt="Horiseon" src={Horiseon} />
-            </span>
-          </div>
-          <div className="carousel-item" id="MK1">
-            <span target="_blank" href="https://govepitr.github.io/">
-              <img alt="MK1" src={MK1} />
-            </span>
-          </div>
-          <div className="carousel-item" id="cinemasearch">
-            <span target="_blank" href="https://govepitr.github.io/GroupProject1/">
-              <img alt="Cinema Search" src={Cinema} />
-            </span>
-          </div>
-          <div className="carousel-item" id="comingsoon">
-            <img src={ComingSoon} alt="coming soon" />
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
+      <div className="scene">
+        <div className="track"><a className="item" href="#0">
+            <div className="title"> </div></a><a className="item" href="#0">
+            <div className="title">Run Buddy</div></a><img alt="Run Buddy" src={RB} /><a className="item" target="_blank" rel="noreferrer" href="https://govepitr.github.io/run-buddy/">
+            <div className="title">Flash Card Buddy</div></a><img alt="FCB" src={FCB} /><a className="item" target="_blank" rel="noreferrer" href="https://flash-card-buddy.herokuapp.com/">
+            <div className="title">Horiseon</div></a><img alt="Horiseon" src={Horiseon} /><a className="item" target="_blank" rel="noreferrer" href="https://govepitr.github.io/Horiseon/">
+            <div className="title">Portfolio MK1</div></a><img alt="MK1" src={MK1} /><a className="item" target="_blank" rel="noreferrer" href="https://govepitr.github.io/">
+            <div className="title">Cinema Search</div></a><img alt="Cinema Search" src={Cinema} /><a className="item" target="_blank" rel="noreferrer" href="https://govepitr.github.io/GroupProject1/">
+            <div className="title"></div></a><img src={ComingSoon} alt="coming soon" /><a className="item" href="#0">
+            <div className="title"></div></a><img src={Pardon} alt="pardon" /><a className="item" href="#0">
+            <div className="title"></div></a><img src={Who} alt="who" /><a className="item" href="#0">
+            <div className="title"></div></a><img src={Shards} alt="shards"/><a className="item" target="_blank" rel="noreferrer" href="https://www.thisworldthesedays.com/flowing-shards.html">
+            <div className="title"></div></a></div>
       </div>
     </section>
     </section>
